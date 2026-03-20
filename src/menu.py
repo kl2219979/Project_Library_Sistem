dbf74e076cfb3875b67c1a680d34ad092575bc07
IbrahimElopez <expo.ebratt.1997@gmail.com>
2026-03-19 20:36:21 -0500

    Menu and Validation features

--- src/menu.py
+++ src/menu.py
@@ -1,4 +1,30 @@
 # Menu
 
 def menu():
-    pass
+    while True:
+        print("Menu options: ")
+        print("1 . Agregar")
+        print("2 . Listar")
+        print("3 . Actualizar")
+        print("4 . Eliminar")
+        print("5 . Disponibilidad")
+        print("6 . Salir")
+        entrada_opcion = int(input("Escoge una opcion: "))
+        match entrada_opcion:
+            case 1: 
+                print("agregar")
+            case 2: 
+                print("Listar")
+            case 3: 
+                print("Actualizar")
+            case 4: 
+                print("Eliminar")
+            case 5: 
+                print("Disponibilidad")
+            case 6: 
+                print("Saliendo")
+                break
+            case _:
+                print("Eliga una opcion correcta")
+menu()
+    
