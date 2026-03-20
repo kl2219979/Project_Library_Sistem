9afd58da24f25aefb562b8eb034c861746d4f330
Melissa <melisofirb@gmail.com>
2026-03-19 20:32:55 -0500

    created funtions to update book

--- src/validations.py
+++ src/validations.py
@@ -1,11 +1,13 @@
 # validaciones
-def validacion_nombre_libro():
-    pass
-def validacion_año_libro():
-    pass
-
-def validacion_autor_libro():
-    pass
-
-def validacion_opcion_menu():
-    pass
+def validation_update():
+    try: 
+        selected_book= int(input("\nWhich book do you want to update (select the number): "))
+        if 0 < selected_book <= len(books):
+            return selected_book
+        else: 
+            print("\nError, try again")
+            return validation_update()
+        
+    except ValueError:
+        print("Error, try again")
+        return validation_update()
