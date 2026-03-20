685928c5de65b2dde7208ff5ed19d45d523d7b2c
Sbenavides14 <sbenavidesd002@gmail.com>
2026-03-19 20:01:56 -0500

    The functions to register books and display books were created.

--- src/services.py
+++ src/services.py
@@ -1,16 +1,24 @@
+
 # Funciones 
 
-def registrar_libro():
-    pass
+def register_book():
+    """This function requests user data such as (tittle, year, author) and then adds it to the dictionary as a list"""
+    
+    tittle = input("Enter the book tittle: ")
+    year = int(input("Enter the year of the book: "))
+    author = input("Enter the book's author:")
+    return {
+        "tittle": tittle,
+        "year": year,
+        "author": author
+    }
 
-def mostrar_libros():
-    pass
 
-def actualizar_libro():
-    pass
+def show_books():
+    """This function displays the tittles in the list"""
+    
+    for i, each_book in enumerate(books):
+        print(f"{i+1} - tittle: {each_book['tittle']}, year: {each_book['year']}, author: {each_book['author']} ")
+    
+    
 
-def eliminar_libro():
-    pass
-
-def marcar_libro_prestado():
-    pass
